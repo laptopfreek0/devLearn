e3c6dfe1b6ada7b40317e2a2b780d82c9802cf74
Jonathan <laptopfreek0@gmail.com>
2014-05-31 15:20:09 -0400

    Removed some unused imports

--- src/com/hackanooga/devLearn/MainActivity.java
+++ src/com/hackanooga/devLearn/MainActivity.java
@@ -3,22 +3,18 @@ package com.hackanooga.devLearn;
 import java.util.Locale;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
