642660750ddd111c5b2fea5172a71f7d639bb669
Jonathan <laptopfreek0@gmail.com>
2014-05-31 23:19:45 -0400

    Oh please don't look. Too many code sins

--- src/com/hackanooga/devLearn/LessonActivity.java
+++ src/com/hackanooga/devLearn/LessonActivity.java
@@ -1,7 +1,15 @@
 package com.hackanooga.devLearn;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LessonActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_lesson);
+	    String lesson_text = this.getIntent().getStringExtra(LessonListActivity.lesson_text);
+	    String language_text = this.getIntent().getStringExtra(LanguageListActivity.language_text);
+	}
 
 }
--- src/com/hackanooga/devLearn/LessonListActivity.java
+++ src/com/hackanooga/devLearn/LessonListActivity.java
@@ -13,44 +13,31 @@ import android.widget.TextView;
 public class LessonListActivity  extends Activity {
 	
 	public static String lesson_id = "lesson_id";
+	public static String lesson_text = "lesson_text";
+	
+	private String[] lessonList;
+	private String language_text = "";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_lesson_list);
 	    
-	    int lesson_array_id = this.getIntent().getIntExtra(LanguageListActivity.language_id, -1);
-	    String language_text = this.getIntent().getStringExtra(LanguageListActivity.language_text);
+	    final int lesson_array_id = this.getIntent().getIntExtra(LanguageListActivity.language_id, -1);
+	    language_text = this.getIntent().getStringExtra(LanguageListActivity.language_text);
 	    TextView tv = (TextView) findViewById(R.id.textView1);
 	    tv.setText(tv.getText() + " " + language_text);
 	    
-	    String[] LanguageList = this.getResources().getStringArray(lesson_array_id);
+	    lessonList = this.getResources().getStringArray(lesson_array_id);
 	    ListView lv = (ListView) findViewById(R.id.lesson_list_view);
-	    lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, LanguageList));
+	    lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lessonList));
 	    lv.setOnItemClickListener(new OnItemClickListener() {
 		    @Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-/*				Intent intent = new Intent(LessonListActivity.this, LessonActivity.class);
-				switch (arg2) {
-					case 0:
-					default:
-						//html
-						intent.putExtra(language_id, R.array.html_lessons);
-						break;
-					case 1:
-						// css
-						intent.putExtra(language_id, R.array.css_lessons);
-						break;
-					case 2:
-						// javascript
-						intent.putExtra(language_id, R.array.javascript_lessons);
-						break;
-					case 3:
-						// php
-						intent.putExtra(language_id, R.array.php_lessons);
-						break;
-				}
-				startActivity(intent);*/
+		    	Intent intent = new Intent(LessonListActivity.this, LessonActivity.class);
+	    		intent.putExtra(lesson_text, lessonList[arg2]);
+	    		intent.putExtra(LanguageListActivity.language_text, language_text);
+		    	startActivity(intent);
 			 }
 	    });
 	}
