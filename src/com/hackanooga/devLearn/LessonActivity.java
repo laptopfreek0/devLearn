206d6e1b4592ad87f1ad5be1548f6387a85e72db
Jonathan <laptopfreek0@gmail.com>
2014-06-01 00:02:18 -0400

    Added Language and lesson to the lesson page

--- src/com/hackanooga/devLearn/LessonActivity.java
+++ src/com/hackanooga/devLearn/LessonActivity.java
@@ -2,6 +2,7 @@ package com.hackanooga.devLearn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LessonActivity extends Activity {
 	@Override
@@ -10,6 +11,9 @@ public class LessonActivity extends Activity {
 	    setContentView(R.layout.activity_lesson);
 	    String lesson_text = this.getIntent().getStringExtra(LessonListActivity.lesson_text);
 	    String language_text = this.getIntent().getStringExtra(LanguageListActivity.language_text);
+	    
+	    TextView text = (TextView) findViewById(R.id.textTitle);
+	    text.setText(language_text + ": " + lesson_text);
 	}
 
 }
