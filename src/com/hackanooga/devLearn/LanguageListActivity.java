5733a52b173b30774a164c9dfff2de8af8c9eccd
Jonathan <laptopfreek0@gmail.com>
2014-05-31 22:51:44 -0400

    Fixed the second Language page

--- src/com/hackanooga/devLearn/LanguageListActivity.java
+++ src/com/hackanooga/devLearn/LanguageListActivity.java
@@ -1,36 +1,53 @@
 package com.hackanooga.devLearn;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class LanguageListActivity  extends Activity {
+	
+	public static String language_id = "language_id";
+	public static String language_text = "language_text";
+	String[] languageList;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_language_list);
 	    
-	    String[] LanguageList = this.getResources().getStringArray(R.array.languages);
+	    languageList= this.getResources().getStringArray(R.array.languages);
 	    ListView lv = (ListView) findViewById(R.id.language_list_view);
-	    lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, LanguageList));
+	    lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, languageList));
 	    lv.setOnItemClickListener(new OnItemClickListener() {
 		    @Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-				Context context = getApplicationContext();
-				CharSequence text = String.valueOf(arg2);
-				int duration = Toast.LENGTH_SHORT;
-		
-				Toast toast = Toast.makeText(context, text, duration);
-				toast.show();
-				
+				Intent intent = new Intent(LanguageListActivity.this, LessonListActivity.class);
+				intent.putExtra(language_text, languageList[arg2]);
+				switch (arg2) {
+					case 0:
+					default:
+						//html
+						intent.putExtra(language_id, R.array.html_lessons);
+						break;
+					case 1:
+						// css
+						intent.putExtra(language_id, R.array.css_lessons);
+						break;
+					case 2:
+						// javascript
+						intent.putExtra(language_id, R.array.javascript_lessons);
+						break;
+					case 3:
+						// php
+						intent.putExtra(language_id, R.array.php_lessons);
+						break;
+				}
+				startActivity(intent);
 			 }
 	    });
 	}
--- src/com/hackanooga/devLearn/LessonListActivity.java
+++ src/com/hackanooga/devLearn/LessonListActivity.java
@@ -1,40 +1,57 @@
 package com.hackanooga.devLearn;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class LessonListActivity  extends Activity {
 	
-	public ListView lv;
+	public static String lesson_id = "lesson_id";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		  String[] myList = new String[] {"Hello","World","Foo","Bar", "Life", "Is", "Good", "Zup", "zup zup", "zup zup zup"};
-	        lv = new ListView(this);
-	        lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,myList));
-	        setContentView(lv);
-	        lv.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				Context context = getApplicationContext();
-				CharSequence text = String.valueOf(arg2);
-				int duration = Toast.LENGTH_SHORT;
-
-				Toast toast = Toast.makeText(context, text, duration);
-				toast.show();
-				//
-				
-			}
-	     });
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_lesson_list);
+	    
+	    int lesson_array_id = this.getIntent().getIntExtra(LanguageListActivity.language_id, -1);
+	    String language_text = this.getIntent().getStringExtra(LanguageListActivity.language_text);
+	    TextView tv = (TextView) findViewById(R.id.textView1);
+	    tv.setText(tv.getText() + " " + language_text);
+	    
+	    String[] LanguageList = this.getResources().getStringArray(lesson_array_id);
+	    ListView lv = (ListView) findViewById(R.id.lesson_list_view);
+	    lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, LanguageList));
+	    lv.setOnItemClickListener(new OnItemClickListener() {
+		    @Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+/*				Intent intent = new Intent(LessonListActivity.this, LessonActivity.class);
+				switch (arg2) {
+					case 0:
+					default:
+						//html
+						intent.putExtra(language_id, R.array.html_lessons);
+						break;
+					case 1:
+						// css
+						intent.putExtra(language_id, R.array.css_lessons);
+						break;
+					case 2:
+						// javascript
+						intent.putExtra(language_id, R.array.javascript_lessons);
+						break;
+					case 3:
+						// php
+						intent.putExtra(language_id, R.array.php_lessons);
+						break;
+				}
+				startActivity(intent);*/
+			 }
+	    });
 	}
-
 }
